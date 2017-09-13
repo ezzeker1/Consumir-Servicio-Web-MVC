262c82202f679ac028f0f0cc6172dd66a7d9347a
Bryan Omar Ramirez Chavez <bryan@ramirezchavez.net>
2017-09-13 14:07:39 -0500

    Primera version
    conexion al servicio.
    envio de correos.
    manipulacion de data

---
+++ Scripts/propio/consulta.js
@@ -0,0 +1,27 @@
+﻿function Accion() {
+    var item = Mapper();
+    var tdocumento = $("#IdTDocumento").val();
+    var mail = $("#mail").val();
+    if (item != "" && mail != "" && tdocumento != "") {
+        $("#clickme").attr('disabled', 'disabled');
+        $.ajax({
+            url: "Cotizacion/Consulta",
+            type: "GET",
+            data: { item: item, tdocumento: tdocumento, mail: mail },
+            success: function (data) {
+              
+                $("#renderBody").html(data);
+                $("#clickme").removeAttr('disabled');
+            }
+        });
+    } else {
+
+        alert("Debe de ingresar el DNI de documento y Email!!!...");
+
+    }
+};
+function Mapper() {
+    var obj = 1;
+    obj = $("#dni").val();
+    return obj;
+};
